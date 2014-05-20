28cecec9fd9066936860f3e516e845334e60ddac
Joshua Hoblitt <jhoblitt@cpan.org>
2014-05-20 13:58:58 -0700

    prune mdadmversion fact value
    Instead of returning the literal string returned by `mdadm --version`,
    parse out and return only the version number.

--- lib/facter/mdadmversion.rb
+++ lib/facter/mdadmversion.rb
@@ -3,7 +3,11 @@ Facter.add('mdadmversion') do
   setcode do
     unless mdadm.nil?
       # `mdadm --version` sends it's output to stderr
-      Facter::Util::Resolution.exec("#{mdadm} --version 2>&1")
+      output = Facter::Util::Resolution.exec("#{mdadm} --version 2>&1")
+      next if output.nil?
+      m = output.match(/mdadm - v([\d\.]+) -/)
+      next unless m.size == 2
+      m[1]
     end 
   end
 end
--- spec/unit/facts/mdadmversion_spec.rb
+++ spec/unit/facts/mdadmversion_spec.rb
@@ -3,14 +3,36 @@ require 'spec_helper'
 describe 'mdadmversion', :type => :fact do
   before(:each) { Facter.clear }
 
-  context 'mdadm fact => /sbin/madam' do
-    it 'should get the version string' do
-      Facter.fact(:mdadm).stubs(:value).returns('/sbin/mdadm')
-      Facter::Util::Resolution.stubs(:exec).with('/sbin/mdadm --version 2>&1').returns('mdadm - v3.2.3 - 23rd December 2011')
+  context 'el5.x versions' do
+    context 'mdadm fact => /sbin/madam' do
+      it 'should get the version string' do
+        Facter.fact(:mdadm).stubs(:value).returns('/sbin/mdadm')
+        Facter::Util::Resolution.stubs(:exec).with('/sbin/mdadm --version 2>&1').returns('mdadm - v2.6.9 - 10th March 2009')
 
-      Facter.fact(:mdadmversion).value.should == 'mdadm - v3.2.3 - 23rd December 2011'
+        Facter.fact(:mdadmversion).value.should == '2.6.9'
+      end
     end
-  end
+  end # el5.x versions
+
+  context 'el6.x versions' do
+    context 'mdadm fact => /sbin/madam' do
+      it 'should get the version string' do
+        Facter.fact(:mdadm).stubs(:value).returns('/sbin/mdadm')
+        Facter::Util::Resolution.stubs(:exec).with('/sbin/mdadm --version 2>&1').returns('mdadm - v3.2.3 - 23rd December 2011')
+
+        Facter.fact(:mdadmversion).value.should == '3.2.3'
+      end
+    end
+
+    context 'mdadm fact => /sbin/madam' do
+      it 'should get the version string' do
+        Facter.fact(:mdadm).stubs(:value).returns('/sbin/mdadm')
+        Facter::Util::Resolution.stubs(:exec).with('/sbin/mdadm --version 2>&1').returns('mdadm - v3.2.6 - 25th October 2012')
+
+        Facter.fact(:mdadmversion).value.should == '3.2.6'
+      end
+    end
+  end # el6.x versions
 
   context 'mdadm fact not set' do
     it 'should return nil' do
